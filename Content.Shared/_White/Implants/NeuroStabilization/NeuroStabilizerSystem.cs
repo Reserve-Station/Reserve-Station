4b7d373f027fdf80fe6d22e81a73c03c451e794d
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-07-06 13:10:11 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/_White/Implants/NeuroStabilization/NeuroStabilizerSystem.cs
+++ Content.Shared/_White/Implants/NeuroStabilization/NeuroStabilizerSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Neverluckz <yanechurka1000.7@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Electrocution;
 using Content.Shared.Damage.Systems;
 using Content.Shared.Damage.Events;
