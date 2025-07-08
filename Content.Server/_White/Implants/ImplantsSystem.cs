e1daddb8f41eb341d29365c2585635ee4c7389c0
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-07-08 12:24:20 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_White/Implants/ImplantsSystem.cs
+++ Content.Server/_White/Implants/ImplantsSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Neverluckz <yanechurka1000.7@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.Administration.Logs;
 using Content.Server.Chat.Managers;
 using Content.Server.Mind;
