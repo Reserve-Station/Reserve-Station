a23481b0d92e315f3f56cabf188186e02d6620cd
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-07-10 10:54:10 +0000

    chore: Automatically update REUSE headers

--- Content.Server/GameTicking/Rules/Components/TraitorRuleComponent.cs
+++ Content.Server/GameTicking/Rules/Components/TraitorRuleComponent.cs
@@ -22,6 +22,7 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Kutosss <162154227+Kutosss@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 MilenVolf <63782763+MilenVolf@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 ReserveBot <211949879+ReserveBot@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Solstice <solsticeofthewinter@gmail.com>
 // SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
 // SPDX-FileCopyrightText: 2025 Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
