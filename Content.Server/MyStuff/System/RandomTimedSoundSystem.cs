e8831a94de05542cabdb79df4824dff4eafe3127
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-08-03 10:30:36 +0000

    chore: Automatically update REUSE headers

--- Content.Server/MyStuff/System/RandomTimedSoundSystem.cs
+++ Content.Server/MyStuff/System/RandomTimedSoundSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Dedi1984 <adrian.preobrazhenskiy@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Shared.Audio;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
