daba74b09bfea89fbaffac5d6d78b36b84f53bee
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-09-19 19:35:14 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Ghostbar/AntagBaseSystem.cs
+++ Content.Goobstation.Server/Ghostbar/AntagBaseSystem.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <57039557+Ilya246@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Kutosss <162154227+Kutosss@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 ReserveBot <211949879+ReserveBot@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Scruq445 <storchdamien@gmail.com>
 // SPDX-FileCopyrightText: 2025 Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 crasg <109207982+Scruq445@users.noreply.github.com>
