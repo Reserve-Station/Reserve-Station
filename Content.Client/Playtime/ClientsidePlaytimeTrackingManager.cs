0a8d30b34147b2cccf434cecd103c2ffa05e2352
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-11-10 22:22:18 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Playtime/ClientsidePlaytimeTrackingManager.cs
+++ Content.Client/Playtime/ClientsidePlaytimeTrackingManager.cs
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Kirill <kirill@example.com>
+// SPDX-FileCopyrightText: 2025 ReserveBot <211949879+ReserveBot@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Rouden <149893554+Roudenn@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 deathride58 <deathride58@users.noreply.github.com>
 //
