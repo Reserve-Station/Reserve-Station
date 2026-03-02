0b551a0ceeabdba36989780176c6325154644fce
Svarshik <lexasvarshik666@gmail.com>
2026-03-02 22:43:44 +0500

    db migration

---
+++ Content.Server.Database/Migrations/Postgres/20260302174207_PendingChanges.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class PendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "uplink",
+                table: "profile");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "uplink",
+                table: "profile",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
